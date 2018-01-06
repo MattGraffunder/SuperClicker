326920eeff4e185c186feffe6a071eb05d503bf5
MattGraffunder <MattGraffunder@users.noreply.github.com>
2018-01-06 14:11:08 -0600

    Remove unused Using statements

--- src/SuperClicker/ClickerView.cs
+++ src/SuperClicker/ClickerView.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System;
-using System.Runtime.InteropServices;
-using System.ComponentModel;
-using System.Threading;
 using SuperClicker.Core.ClickStrategies;
 using SuperClicker.Core;
 using WindowsInterface;
